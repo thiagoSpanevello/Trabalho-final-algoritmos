0b80e5a0ceaea80d266ac246987f5da9b46db52b
luigi pisoni <luigipisoni04@gmail.com>
2023-06-21 21:15:05 -0300

    novas funcoes

--- demo.py
+++ demo.py
@@ -1,19 +1,22 @@
 class Aluno:
-    nome = None
-    cpf = None
-    peso = None
-    altura = None
-    status = False
+    def __init__(self, nome, cpf, peso, altura, status):
+        self.nome = nome
+        self.cpf = cpf
+        self.peso = peso
+        self.altura = altura
+        self.status = status
 
 
 class Exercicio:
-    nome = None
-    numRep = 0
-    pesoExec = 0
+    def __init__(self, nome, numRep, pesoExec):
+        self.nome = nome
+        self.numRep = numRep
+        self.pesoExec = pesoExec
 
 
-listaAlunos = []
-listaExerc = []
+listaAlunos = [Aluno('jorge', 39552078, 65, 165, False)]
+
+listaTreinos = [[Exercicio('Flexão', 12, 50)]]
 
 
 def menu():
@@ -38,7 +41,7 @@ def menu():
                 aluno = aluno = buscaAlunoNome(nomeAluno)
                 if (aluno != -1):
                     idAluno = listaAlunos.index(aluno)
-                    return menuTreino()
+                    return menuTreino(idAluno)
                 else:
                     print('Aluno não encontrado')
 
@@ -46,12 +49,20 @@ def menu():
                 nomeAluno = input('Digite o nome do aluno: ')
                 aluno = buscaAlunoNome(nomeAluno)
                 if (aluno != -1):
-                    print('------ ALUNO ------')
-                    print('Nome:', aluno.nome, '\nCPF:', aluno.cpf,
-                          '\nPeso:', aluno.peso, '\nAltura:', aluno.altura)
-                    print('----- TREINO -----')
+                    print('\n------ ALUNO ------')
+                    printAluno(aluno)
+
+                    idTreino = listaAlunos.index(aluno)
+                    print('\n----- TREINO -----')
+                    treino = listaTreinos[idTreino]
+                    if (not treino):
+                        print('O aluno não possui treino')
+                    else:
+                        for exer in treino:
+                            printExer(exer)
+
                 else:
-                    print('Aluno não encontrado')
+                    print('\nAluno não encontrado')
 
             case 6:  # entra no menu de relatórios
                 return relatorio()
@@ -82,8 +93,8 @@ def nomeValido(input):  # verifica se o nome é válido (!número e >3 letras)
         float(input)  # se o input puder ser um float, é um número
         return '\n(!) Nome não pode ser um número\n'
     except ValueError:
-        if (len(input) <= 3):
-            return '\n(!) Nome precisa ter mais de 3 letras\n'
+        if (len(input) <= 2):
+            return '\n(!) Nome precisa ter mais de 2 letras\n'
         else:
             return True
 
@@ -91,20 +102,19 @@ def nomeValido(input):  # verifica se o nome é válido (!número e >3 letras)
 # ALUNO -------------------------------------------------------
 
 def novoAluno():  # --- CASO 1 ---
-    new = Aluno()
-
     verificacao = False
+    nome = ''
     while (verificacao != True):
-        new.nome = input('Nome do aluno: ')
-        verificacao = nomeValido(new.nome)
+        nome = input('Nome do aluno: ')
+        verificacao = nomeValido(nome)
         if (verificacao != True):  # se o nome for inválido
             print(verificacao)  # printa a mensagem dizendo o que tá errado
 
-    new.cpf = input('CPF do aluno: ')
-    new.peso = input('Peso do aluno: ')
-    new.altura = input('Altura do aluno: ')
-    listaAlunos.append(new)
-    listaExerc.append([])
+    cpf = input('CPF do aluno: ')
+    peso = input('Peso do aluno: ')
+    altura = input('Altura do aluno: ')
+    listaAlunos.append(Aluno(nome, cpf, peso, altura, False))
+    listaTreinos.append([])
     print('Aluno cadastrado com sucesso')
     return menu()
 
@@ -125,23 +135,17 @@ def relatorio():  # --- CASO 6 ---
         case 1:
             print('------ LISTA ------')
             for aluno in listaAlunos:
-                print('Nome:', aluno.nome, '\nCPF:', aluno.cpf,
-                      '\nPeso:', aluno.peso, '\nAltura:', aluno.altura)
-                print('     -----')
+                printAluno(aluno)
         case 2:
             print('------ LISTA ------')
             for aluno in listaAlunos:
                 if aluno.status == True:
-                    print('Nome:', aluno.nome, '\nCPF:', aluno.cpf,
-                          '\nPeso:', aluno.peso, '\nAltura:', aluno.altura)
-                    print('     -----')
+                    printAluno(aluno)
         case 3:
             print('------ LISTA ------')
             for aluno in listaAlunos:
                 if aluno.status == False:
-                    print('Nome:', aluno.nome, '\nCPF:', aluno.cpf,
-                          '\nPeso:', aluno.peso, '\nAltura:', aluno.altura)
-                    print('     -----')
+                    printAluno(aluno)
         case _:
             print('Opção inválida')
     return menu()
@@ -154,9 +158,16 @@ def buscaAlunoNome(nome):  # busca um aluno pelo nome
     return -1  # se não retornar com o aluno, quer dizer que não existe
 
 
+def printAluno(aluno):
+    print('Nome:', aluno.nome, '\nCPF:', aluno.cpf,
+          '\nPeso:', aluno.peso, '\nAltura:', aluno.altura,
+          '\nStatus:', 'Ativo' if aluno.status == True else 'Inativo')
+    print('     -----')
+
 # TREINO -------------------------------------------------------
 
-def menuTreino():
+
+def menuTreino(idAluno):
     print('Menu de Treino')
     print('  1 - Cadastrar novo exercício')
     print('  2 - Alterar exercício')
@@ -166,32 +177,32 @@ def menuTreino():
     print()
     match caso:
         case 1:
-            print('n ta feito')
+            return novoExercicio(idAluno)
         case _:
             print('Opção inválida')
 
 
 def novoExercicio(idAluno):
-    x = int(input('Digite 9 para sair'))
-    while True:
-        if x == 9:
-            break
-        else:
-            exer = Exercicio()
-            exer.nome = input('Nome do exercício: ')
-            exer.numRep = int(input('Numero de repetições: '))
-            exer.pesoExec = input('Peso usado no exercício: ')
-            if exer in listaExerc[idAluno]:
-                print('Exercício já existente na lista')
-                return menu()
-            else:
-                listaExerc[idAluno].append(exer)
-                print('Exercício adicionado com sucesso')
-                # testa o id dos alunos ate achar o correto e seta o status pra true
-                for i in range(listaAlunos):
-                    if i == idAluno:
-                        listaAlunos[i].status == True
+    nome = input('Nome do exercício: ')
+    numRep = int(input('Numero de repetições: '))
+    pesoExec = input('Peso usado no exercício: ')
+    exer = Exercicio(nome, numRep, pesoExec)
+    if exer in listaTreinos[idAluno]:
+        print('Exercício já existente na lista')
+        return menu()
+    else:
+        listaTreinos[idAluno].append(exer)
+        print('Exercício adicionado com sucesso')
+        # testa o id dos alunos ate achar o correto e seta o status pra true
+        listaAlunos[idAluno].status = True
     return menu()
+
+
+def printExer(exer):
+    print('Exercício:', exer.nome,
+          '\nRepetições:', exer.numRep,
+          '\nPeso:', exer.pesoExec)
+    print('     -----')
 
 
 def main():
